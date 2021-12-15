cf702b5e6ff93a266645e8616d066c5556151a06
KwakYV <kvakyur@gmail.com>
2021-12-15 20:15:12 +0300

    Revert "Implemented Cart entity save operation"
    This reverts commit 64246c26b6960e4f4f8360969ed75c6e03c382d8.

--- gb-thymeleaf/src/main/java/ru/gb/gbthymeleaf/controller/ProductController.java
+++ gb-thymeleaf/src/main/java/ru/gb/gbthymeleaf/controller/ProductController.java
@@ -7,14 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import ru.gb.gbthymeleaf.entity.Cart;
 import ru.gb.gbthymeleaf.entity.Product;
-import ru.gb.gbthymeleaf.service.CartService;
 import ru.gb.gbthymeleaf.service.ProductService;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,8 +18,6 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService productService;
-    private final CartService cartService;
-    private Cart cart;
 
     @GetMapping("/all")
     public String getProductList(Model model) {
@@ -49,24 +43,6 @@ public class ProductController {
         productService.save(product);
         return "redirect:/product/all";
     }
-
-    @GetMapping("/cart")
-    public String addToCart(@RequestParam(name = "id", required = false) Long id){
-        Product product = productService.findById(id);
-        List<Product> products = new ArrayList<>();
-        products.add(product);
-        if (cart == null){
-            cart = Cart.builder()
-                    .number(cartService.maxNumber() + 1)
-                    .products(products)
-                    .build();
-        } else {
-            cart.getProducts().add(product);
-        }
-        cart = cartService.saveOrUpdate(cart);
-        return "redirect:/product/all";
-    }
-
 
     @GetMapping("/delete")
     public String deleteById(@RequestParam(name = "id") Long id) {
