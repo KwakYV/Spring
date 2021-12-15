8a6a402232b952af0c7277a87e78edda99a8b3b6
KwakYV <kvakyur@gmail.com>
2021-12-15 20:15:12 +0300

    Revert "Implemented Cart entity save operation"
    This reverts commit 64246c26b6960e4f4f8360969ed75c6e03c382d8.

--- gb-thymeleaf/src/main/resources/db/ddl.sql
+++ gb-thymeleaf/src/main/resources/db/ddl.sql
@@ -14,24 +14,6 @@ CREATE TABLE HW8.PRODUCT
     UNIQUE (TITLE)
 );
 
-create table hw8.cart(
-   id bigserial not null primary key ,
-   cart_number bigint not null
-);
-alter table hw8.cart owner to geek_user;
-
-create table hw8.ln_cart_product(
-    cart_id bigint not null constraint fk_cart references hw8.cart,
-    product_id bigint not null constraint fk_product references hw8.product,
-    quantity bigint not null,
-
-    constraint pk_ln_cart_product primary key (cart_id, product_id)
-
-);
-alter table hw8.ln_cart_product owner to geek_user;
-
-
+DROP TABLE PRODUCT;
 
 SELECT * FROM hw8.product;
-select * from hw8.cart;
-select * from hw8.ln_cart_product;
