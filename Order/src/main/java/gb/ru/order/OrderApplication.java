b65b4f2bc672cd03bf06d5d4beb8efa52608375c
yuriy <kvakyur@gmail.com>
2022-01-10 22:46:33 +0300

    gradle settings

---
+++ Order/src/main/java/gb/ru/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package gb.ru.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class, args);
+    }
+
+}
